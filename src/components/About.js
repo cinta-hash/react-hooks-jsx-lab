90e558385811a9f209564a77ad272fe09def4e60
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-01-27 14:10:27 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+  <h2>About Me</h2>
+  <p>A developer is an individual that builds and create software and applications. He or she writes, debugs and executes the source code of a software application. A developer is also known as a software developer, computer programmer, programmer, software coder or software engineer</p>
+  <img src = { image} alt = "I made this"/>
+  </div>;
 }
 
 export default About;
